16ed832a01204b0d76afc7c620fc5acacdcb52ed
Zack <zpotts@pac.company>
2024-01-28 11:01:59 -0700

    added headliny thng

--- app/components/Teaser.tsx
+++ app/components/Teaser.tsx
@@ -2,14 +2,18 @@
 const Teaser = () => {
     return(
         <div id="teaser">
-        <div className="rounded-xl border-2 border-orange-dark pt-2 px-4 mb-4">
             <div className="font-normal text-lg">
-            <p className="mb-4">On the edge of Scottsdale, where city meets desert, a home that&apos;s hosted <strong>artist receptions and private piano performances</strong>. That is, when it&apos;s not the staging point for <strong>motocross rides and cycling tours</strong>.</p>
+                <p className="mb-4 font-semibold uppercase"><strong>outdoors and adventure travel &bull; family vacations &bull; weddings &amp; engagements &bull; business travel</strong></p>
+             </div>
+            <div className="rounded-xl border-2 border-orange-dark pt-2 px-4 mb-4">
+                <div className="font-normal text-lg">
+                
+                <p className="mb-4">On the edge of Scottsdale, where city meets desert, a home that&apos;s hosted <strong>artist receptions and private piano performances</strong>. That is, when it&apos;s not the staging point for <strong>motocross rides and cycling tours</strong>.</p>
 
-            <p className="mb-4">A venue that is just as perfect for <strong>rooftop weddings</strong> as it is for <strong>relaxing on the putting green</strong> with a beer in your hand.</p>
+                <p className="mb-4">A venue that is just as perfect for <strong>rooftop weddings</strong> as it is for <strong>relaxing on the putting green</strong> with a beer in your hand.</p>
 
-            <p className="mb-4">A short drive &ndash; or ride &ndash; to the town of <strong>Cave Creek</strong> and legendary spots like <strong>The Buffalo Chip</strong> and <strong>El Encanto</strong>. But when you are sitting by the koi pond, watching the lights come on across the Valley of the Sun, you won&apos;t want to leave for the night, if ever.</p>
-            </div>
+                <p className="mb-4">A short drive &ndash; or ride &ndash; to the town of <strong>Cave Creek</strong> and legendary spots like <strong>The Buffalo Chip</strong> and <strong>El Encanto</strong>. But when you are sitting by the koi pond, watching the lights come on across the Valley of the Sun, you won&apos;t want to leave for the night, if ever.</p>
+                </div>
         </div>
         <h2 className="text-4xl py-2 font-rye text-orange-dark">Welcome to the Compound</h2>
         </div>
